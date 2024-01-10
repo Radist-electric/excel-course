4a6130f7fae903061b941d32ca5c737532255ffe
Radist <radist-electric@yandex.ru>
2024-01-10 22:01:49 +0500

    webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
 	entry: {
-		main: './index.js'
+		main: [
+			'core-js/stable',
+			'regenerator-runtime/runtime',
+			'./index.js'
+		],
 	},
 	output: {
 		path: path.resolve(__dirname, 'build'),
@@ -22,6 +27,9 @@ module.exports = {
 					to: path.resolve(__dirname, 'build') 
 				},
 			],
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].bundle.css'
 		})
 	],
 	resolve: {
@@ -30,5 +38,27 @@ module.exports = {
 			'@': path.resolve(__dirname, 'src'),
 			'@core': path.resolve(__dirname, 'src', 'core'),
 		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}
+		],
 	}
 }

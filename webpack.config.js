7316a8e0d8fbbe28f6aa388f72f757f6792f2f62
Radist <radist-electric@yandex.ru>
2024-01-10 21:36:05 +0500

    webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -8,5 +10,25 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'build'),
 		filename: '[name].bundle.js'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html'
+		}),
+		new CopyPlugin({
+			patterns: [
+				{ 
+					from: path.resolve(__dirname, 'src', 'favicon.ico'), 
+					to: path.resolve(__dirname, 'build') 
+				},
+			],
+		})
+	],
+	resolve: {
+		extensions: ['.js'],
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@core': path.resolve(__dirname, 'src', 'core'),
+		}
 	}
 }

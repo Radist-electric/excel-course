4ed5853344d8b19875f339fb9453545668617644
Radist <radist-electric@yandex.ru>
2024-01-10 22:30:28 +0500

    webpack modes

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,75 @@
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
-module.exports = {
-	context: path.resolve(__dirname, 'src'),
-	entry: {
-		main: [
-			'core-js/stable',
-			'regenerator-runtime/runtime',
-			'./index.js'
-		],
-	},
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: '[name].bundle.js'
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './index.html'
-		}),
-		new CopyPlugin({
-			patterns: [
-				{ 
-					from: path.resolve(__dirname, 'src', 'favicon.ico'), 
-					to: path.resolve(__dirname, 'build') 
-				},
+module.exports = (env, argv) => {
+	const isProd = argv.mode === 'production';
+	const isDev = !isProd;
+
+	const filename = ext => isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`
+
+
+	return {
+		context: path.resolve(__dirname, 'src'),
+		devtool: isDev ? 'source-map' : false,
+		entry: {
+			main: [
+				'core-js/stable',
+				'regenerator-runtime/runtime',
+				'./index.js'
 			],
-		}),
-		new MiniCssExtractPlugin({
-			filename: '[name].bundle.css'
-		})
-	],
-	resolve: {
-		extensions: ['.js'],
-		alias: {
-			'@': path.resolve(__dirname, 'src'),
-			'@core': path.resolve(__dirname, 'src', 'core'),
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-					'sass-loader',
+		},
+		output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: filename('js')
+		},
+		plugins: [
+			new CleanWebpackPlugin(),
+			new CopyPlugin({
+				patterns: [
+					{ 
+						from: path.resolve(__dirname, 'src', 'favicon.ico'), 
+						to: path.resolve(__dirname, 'build') 
+					},
 				],
-			},
-			{
-				test: /\.m?js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
+			}),
+			new HtmlWebpackPlugin({
+				template: './index.html'
+			}),
+			new MiniCssExtractPlugin({
+				filename: filename('css')
+			})
+		],
+		resolve: {
+			extensions: ['.js'],
+			alias: {
+				'@': path.resolve(__dirname, 'src'),
+				'@core': path.resolve(__dirname, 'src', 'core'),
+			}
+		},
+		module: {
+			rules: [
+				{
+					test: /\.s[ac]ss$/i,
+					use: [
+						MiniCssExtractPlugin.loader,
+						'css-loader',
+						'sass-loader',
+					],
+				},
+				{
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env']
+						}
 					}
 				}
-			}
-		],
+			],
+		}
 	}
 }
